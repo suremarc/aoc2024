6a3694c622378237ce74dc177d079271edd5ee8b
suremarc <8771538+suremarc@users.noreply.github.com>
2024-12-04 17:12:35 -0600

    save work

--- Day01/Main.lean
+++ Day01/Main.lean
@@ -1,6 +1,26 @@
 import Parser
 
-def main : IO Unit :=
-  IO.println "Hello, World!"
+open IO IO.FS.Stream
 
--- abbrev Parser := Parser.SimpleParser
+open Parser Parser.Char.ASCII
+
+def parsePair m [Monad m]: SimpleParserT Substring Char m (Nat × Nat) := do
+  -- TODO: pointfree way to do this?
+  let m ← parseNat
+  dropMany <| whitespace
+  let n ← parseNat
+  return (m, n)
+
+def main : IO Unit := do
+  let stdin ← IO.getStdin
+  let mut sum := 0
+  -- Repeatedly parse lines and run parsePair on each line
+  -- to get a list of (Nat, Nat) pairs
+  let rec loop := do
+    let line ← stdin.getLine
+    if line == "" then
+      IO.println s!"Sum: {sum}"
+    else
+      let (m, n) ← (parsePair.run line)?
+      m + n + loop
+  loop
